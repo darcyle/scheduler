e231a448398e423fa8b0d70b007028fc45483d27
David Grossman <david.grossman@bigfishgames.com>
2015-08-25 21:26:32 -0700

    Fixed the incorrect class name.

--- src/AppBundle/Helper/ApiFactory.php
+++ src/AppBundle/Helper/ApiFactory.php
@@ -4,7 +4,7 @@ namespace AppBundle\Helper;
 
 class ApiFactory {
 	static function getApiByVersion($version) {
-		$className = "\AppBundle\API\ApiV{$version}.php";
+		$className = "\AppBundle\API\ApiV{$version}";
 		if (class_exists($className)) {
 			return new $className;
 		}
