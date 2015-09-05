55e4e77a10be17344780c2059db4cd2e92b7c2e2
David Grossman <david.grossman@bigfishgames.com>
2015-09-05 15:44:07 -0700

    Using a promise for the API.

--- web/js/angular/controllers.js
+++ web/js/angular/controllers.js
@@ -4,32 +4,40 @@ var scheduleControllers = angular.module('scheduleControllers', []);
 
 
 scheduleControllers.controller('ScheduleListCtrl', ['$scope', '$http', 'api', function ($scope, $http, api) {
-	var data  = api('getWeek');
-	$scope.schedules = data.schedules;
-	$scope.weekStartDate = data.weekStartDate;
+	$scope.schedules = [];
+	$scope.weekStartDate = 'fuck';
 
-	console.log(data);
-	console.log($scope);
+	api('getWeek').then(
+		function(data){
+			console.log(data);
+			$scope.schedules = data.schedules;
+			$scope.weekStartDate = data.weekStartDate;
+		},
+		function(error) {
+
+		}
+	);
+
 }]).factory('api', ['$http', function($http) {
 	return function(method, args) {
-   		console.log('API: ', method, args);
-   		if (typeof args == 'undefined') {
-   			args = {};
-   		}
-   		responseData = {}
-	 	$http({
-	        url: '/api/v1/',
-	        method: 'POST',
-	        data: JSON.stringify({'method':method, 'args': args}),
-	        headers: {'Content-Type': 'application/json'}
-	        }).success(function (data, status, headers, config) {
-	        	console.log('data: ',data);
-	        	responseData = data;
-	        }).error(function (data, status, headers, config) {
-	            console.log('ERROR: ' + status)
-	            return false;
-	        });
-	    return responseData;
+		var promise = new Promise(function(resolve, reject){
+			console.log('API: ', method, args);
+			if (typeof args == 'undefined') {
+				args = {};
+			}
+			responseData = {}
+			$http({
+				url: '/api/v1/',
+				method: 'POST',
+				data: JSON.stringify({'method':method, 'args': args}),
+				headers: {'Content-Type': 'application/json'}
+				}).success(function (data, status, headers, config) {
+					resolve(data);
+				}).error(function (data, status, headers, config) {
+					reject(Error(data));
+				});
+		});
+		return promise;
 	};
 }]);
 
