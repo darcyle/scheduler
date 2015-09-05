7ba0e60be324cf692dbea0d4d99903b917f2e61b
aklose <andrew.klose@aklosoakosxl1.local>
2015-09-05 14:44:22 -0700

    Filling out the getDays of week functions

--- src/AppBundle/Helper/Util.php
+++ src/AppBundle/Helper/Util.php
@@ -4,9 +4,27 @@ namespace AppBundle\Helper;
 
 class Util {
 
-	public static function getMonday() {
+	const MONDAY_DAY_OF_WEEK = 1;
+	const FRIDAY_DAY_OF_WEEK = 5;
+
+	public static function getMondayOfSameWeek($date) {
+		return self::getDayOfSameWeek($date, self::MONDAY_DAY_OF_WEEK);
 	}
 
-	public static function getFriday() {
+	public static function getFridayOfSameWeek($date) {
+		return self::getDayOfSameWeek($date, self::FRIDAY_DAY_OF_WEEK);
+	}
+
+	private static function getDayOfSameWeek($date, $dayToFind) {
+		$dateCopy = clone $date;
+		$dayOfWeek = $date->format('w');
+		$offset = $dayToFind - $dayOfWeek;
+		$dateCopy->modify("{$offset} day");
+
+		return $dateCopy;
+	}
+
+	public static function getStartAndEndOfWeek($date) {
+		return [self::getMondayOfSameWeek($date), self::getFridayOfSameWeek($date)];
 	}
 }
