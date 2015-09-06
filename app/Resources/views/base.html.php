11a21f066985cc15b8774f02fa1d0f5cf49e0a41
David Grossman <david.grossman@bigfishgames.com>
2015-09-06 16:14:06 -0700

    Including XDate in the base controller.

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -16,11 +16,12 @@
 			<?php $view['slots']->output('_content') ?>
 		</div>
 
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.min.js"></script>
 		<script src="/js/angular/controllers.js"></script>
 		<script src="/js/angular/filters.js"></script>
 		<script src="/js/angular/app.js"></script>
+		<script src="/js/angular/xdate.js"></script>
 	</body>
 </html>
