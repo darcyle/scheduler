04c91e7b50a42e9ff6e00a8191f1e478c032487e
David Grossman <david.grossman@bigfishgames.com>
2015-09-05 18:20:33 -0700

    Updated the look of the main page. Now correctly display the delete or schedule buttons based on the availability of the appointment.

--- app/Resources/views/schedule/index.html.php
+++ app/Resources/views/schedule/index.html.php
@@ -8,59 +8,52 @@
 	}
 </style>
 
-<div class="jumbotron">
-	<div class="container">
-		<h1>Welcome to the SAG Scheduler!</h1>
-		<p>Here you can easily sign up for massages. All appointments are 30 minutes.</p>
+<div ng-controller="ScheduleListCtrl">
+
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Welcome to the SAG Scheduler!</h1>
+			<p>Here you can easily sign up for massages. All appointments are 30 minutes.</p>
+		</div>
 	</div>
-</div>
 
-<div class="container" ng-controller="ScheduleListCtrl">
-<h1>Week of {{weekStartDate}}</h1>
-<p>10 availabilities</p>
-	<div class="row">
-		<div class="col-md-2">
-			<h1>Mon</h1>
-			<div class="appointments">
-				<div class="panel panel-danger">
-					<div class="panel-heading">10:30AM</div>
-					<div class="panel-body">
-						David Grossman <a href="#" class="btn btn-danger btn-block" ng-click="wtf()">Cancel</a>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2" style="text-align: center">
+				<h3 align="center">{{weekStartDate}}
+				<button class="btn btn-primary" type="button">
+				  Availabilites <span class="badge">5</span>
+				</button>
+				</h3>
+
+				<nav>
+					<ul class="pagination">
+						<li>
+							<a href="#" aria-label="Previous">
+								<span aria-hidden="true">&laquo;</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" aria-label="Next">
+								<span aria-hidden="true">&raquo;</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			
+			<div ng-repeat="schedule in schedules" class="col-md-2">
+				<h1>{{schedule.day}}</h1>
+				<div class="appointments">
+					<div ng-repeat="appointment in schedule.appointments" ng-class="{'panel-success': appointment.user === null, 'panel-danger': appointment.user !== null}" class="panel">
+						<div class="panel-heading">{{appointment.time}}</div>
+						<div class="panel-body">
+							<div class="user">{{appointment.user}}</div>
+							<a ng-if="appointment.user !== null" class="btn btn-danger btn-block" ng-click="cancel(schedule, appointment)">Cancel</a>
+							<a ng-if="appointment.user === null" class="btn btn-primary btn-block" ng-click="schedule(schedule, appointment)">Schedule</a>
+						</div>
 					</div>
 				</div>
-				<div class="panel panel-success">
-					<div class="panel-heading">11:00AM</div>
-					<div class="panel-body">
-						<a href="#" class="btn btn-primary btn-block">Schedule</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-2">
-			<h1>Tue</h1>
-			<div class="appointments">
-				<div ng-repeat="schedule in schedules" class="panel panel-default">
-					<div class="panel-heading">{{schedule.time}}</div>
-					<div class="panel-body">{{schedule.user}}</div>
-				</div>
-			</div>			
-		</div>
-		<div class="col-md-2">
-			<h1>Wed</h1>
-			<div class="appointments">
-
-			</div>
-		</div>
-		<div class="col-md-2">
-			<h1>Thu</h1>
-			<div class="appointments">
-
-			</div>
-		</div>
-		<div class="col-md-2">
-			<h1>Fri</h1>
-			<div class="appointments">
-
 			</div>
 		</div>
 	</div>
